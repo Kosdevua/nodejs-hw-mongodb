d7122ff0a044e0d58e99a071e3681f7d7fc939d7
Kosdevua <konstantin.vodolozhko@gmail.com>
2025-03-31 16:19:07 +0300

    fixed errors with id

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import { isValidObjectId } from 'mongoose';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { getContacts, getContactId } from './services/contacts.js';
 
@@ -16,31 +17,57 @@ export const setupServer = () => {
     }),
   );
   app.get('/contacts', async (req, res) => {
-    const data = await getContacts();
-    res.json({
-      status: 200, // для краси
-      message: 'Successfully found contacts!', // для краси
-      data, // важлива інформація
-    });
+    try {
+      const data = await getContacts();
+      res.json({
+        status: 200, // для краси
+        message: 'Successfully found contacts!', // для краси
+        data, // важлива інформація
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: 'message',
+        error: error.message,
+      });
+    }
   });
 
   app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const data = await getContactId(contactId);
+    try {
+      const { contactId } = req.params;
 
-    if (!data) {
-      return res.status(404).json({
-        status: 404,
-        message: `Contact with ${contactId} not found`,
+      if (!isValidObjectId(contactId)) {
+        return res.status(400).json({
+          status: 400,
+          message: `Invalid contact id format: ${contactId}`,
+        });
+      }
+
+      const data = await getContactId(contactId);
+
+      if (!data) {
+        return res.status(404).json({
+          status: 404,
+          message: `Contact with ${contactId} not found`,
+        });
+      }
+
+      res.json({
+        status: 200,
+        message: `Successfully found contact with ${contactId} id`,
+        data,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: 'Internal server error',
+        error: error.message,
       });
     }
-
-    res.json({
-      status: 200,
-      message: 'Successfully found contact with id',
-      data,
-    });
   });
+
+  ///
 
   app.use((req, res) => {
     res.status(404).json({
