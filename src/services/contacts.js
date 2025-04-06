543821a1bd3326eb4487fcb6b024afa8a90f9c46
Kosdevua <konstantin.vodolozhko@gmail.com>
2025-04-07 00:09:02 +0300

    some code

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,3 +7,21 @@ export const getContactId = (id) => {
   //   throw new Error('Database crashed');
   return contactCollection.findOne({ _id: id });
 };
+
+export const addContact = (payload) => contactCollection.create(payload);
+
+export const updateContact = async (_id, payload, option = {}) => {
+  const { upsert = false } = option; // передаємо третім аргументом
+
+  const rawResult = await contactCollection.findOneAndUpdate({ _id }, payload, {
+    new: true, //щоб додавав оновлений обект
+    upsert, // щоб він додавав або оновлював та додавав
+    includeResultMetadata: true, //повертається повний результат відповіді
+  });
+
+  if (!rawResult || rawResult.value) return null;
+  return {
+    data: rawResult.value,
+    isNew: Boolean(rawResult.lastErrorObject.upserted), //true -   якщо ми додали, false - якщо ми оновили
+  };
+};
