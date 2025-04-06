1877e8a89122596da54b21e39c2f193da21553cb
Kosdevua <konstantin.vodolozhko@gmail.com>
2025-04-06 19:18:48 +0300

    trycatchWrap

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,53 +1,44 @@
+import createHttpError from 'http-errors';
 import { isValidObjectId } from 'mongoose';
 import { getContactId, getContacts } from '../services/contacts.js';
 
-export const getContactsController = async (req, res) => {
-  try {
-    const data = await getContacts();
-    res.json({
-      status: 200, // для краси
-      message: 'Successfully found contacts!', // для краси
-      data, // важлива інформація
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: 500,
-      message: 'message',
-      error: error.message,
-    });
-  }
+export const getContactsController = async (req, res, next) => {
+  const data = await getContacts();
+  res.json({
+    status: 200, // для краси
+    message: 'Successfully found contacts!', // для краси
+    data, // важлива інформація
+  });
 };
 
 export const getContactByIdController = async (req, res) => {
-  try {
-    const { contactId } = req.params;
+  const { contactId } = req.params;
 
-    if (!isValidObjectId(contactId)) {
-      return res.status(400).json({
-        status: 400,
-        message: `Invalid contact id format: ${contactId}`,
-      });
-    }
-
-    const data = await getContactId(contactId);
-
-    if (!data) {
-      return res.status(404).json({
-        status: 404,
-        message: `Contact with ${contactId} not found`,
-      });
-    }
-
-    res.json({
-      status: 200,
-      message: `Successfully found contact with ${contactId} id`,
-      data,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: 500,
-      message: 'Internal server error',
-      error: error.message,
+  if (!isValidObjectId(contactId)) {
+    return res.status(400).json({
+      status: 400,
+      message: `Invalid contact id format: ${contactId}`,
     });
   }
+
+  const data = await getContactId(contactId);
+
+  if (!data) {
+    throw createHttpError(404, `Contact with id=${contactId} not found`);
+
+    // const error = new Error(`Contact with id=${contactId} not found`);
+    // error.status = 404;
+    // throw error;
+
+    // return res.status(404).json({
+    //   status: 404,
+    //   message: `Contact with ${contactId} not found`,
+    // });
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully found contact with id=${contactId} `,
+    data,
+  });
 };
