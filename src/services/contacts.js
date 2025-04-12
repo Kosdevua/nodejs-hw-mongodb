43bda9294b4893a38521745ff106f8756bd8eef1
Kosdevua <konstantin.vodolozhko@gmail.com>
2025-04-13 01:01:24 +0300

    create sort

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,7 +2,14 @@ import contactCollection from '../db/models/Contacts.js';
 
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getContacts = async ({ page, perPage }) => {
+import { SORT_ORDER } from '../constants/index.js';
+
+export const getContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+}) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
@@ -12,7 +19,11 @@ export const getContacts = async ({ page, perPage }) => {
     .merge(contactsQuery)
     .countDocuments();
 
-  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
 
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
 
