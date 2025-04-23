28f5c3da480ff2c53adebe818087f295acc5c72e
Kosdevua <konstantin.vodolozhko@gmail.com>
2025-04-23 21:32:34 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,11 +10,11 @@ import router from './routers/index.js';
 export const setupServer = () => {
   const app = express();
 
-  app.use(router);
-
   app.use(cors());
 
   app.use(express.json());
+
+  app.use(router);
 
   app.use(notFoundHandler);
 
